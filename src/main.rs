1c77b2b315e99061c6da16b85c8201c46d0e3dce
Tóth Gergő <joshplays206@gmail.com>
2022-06-26 12:55:03 +0200

    Hell yeah it do something now!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -8,12 +9,26 @@ fn main() {
 
     println!("The secret number is: {}", &secret_number);
 
-    println!("Please input your guess: ");
+    loop 
+    {  
+        println!("Please input your guess: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+        }
+        }
+    }
 }
